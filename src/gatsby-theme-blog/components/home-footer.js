d98a3a2ce4840ca532d331772945d28dc8ebda0b
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2019-12-17 22:41:41 +0100

    Shadowing component to display RSS url

---
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -0,0 +1,32 @@
+/* Shadowing to add RSS feed link */
+import React, { Fragment } from "react"
+import { Styled, css } from "theme-ui"
+import { Link } from "gatsby"
+
+const Footer = ({ socialLinks }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    © {new Date().getFullYear()}, Powered by
+    {` `}
+    <Styled.a href="https://www.gatsbyjs.org">Gatsby</Styled.a>
+    {` `}&bull;{` `}
+    {socialLinks.map((platform, i, arr) => (
+      <Fragment key={platform.url}>
+        <Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
+          {platform.name}
+        </Styled.a>
+        {arr.length - 1 !== i && (
+          <Fragment>
+            {` `}&bull;{` `}
+          </Fragment>
+        )}
+      </Fragment>
+    ))}
+    <Styled.a as={Link} css={css({float: 'right'})} to="/rss.xml" >RSS</Styled.a>
+  </footer>
+)
+export default Footer
