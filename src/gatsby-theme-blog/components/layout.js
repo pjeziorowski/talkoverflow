e8c0896f7163335060f7d9cf25878f7b603992bb
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2019-12-28 01:40:57 +0100

    Fix padding

--- src/gatsby-theme-blog/components/layout.js
+++ src/gatsby-theme-blog/components/layout.js
@@ -125,7 +125,7 @@ export default ({ children, ...props }) => {
               maxWidth: `container`,
               mx: `auto`,
               px: 3,
-              py: 0,
+              pb: 4,
             })}
           >
             {children}
